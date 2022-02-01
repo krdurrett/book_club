15eae029514861471423529902d4940e2922dafb
Kayla Durrett <kayla.durrett@gmail.com>
2022-02-01 09:46:29 -0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      books: [
+        {
+          title: 'The Name of the Wind',
+          description: 'Tale of Kvothe',
+          image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodreads.com%2Fbook%2Fshow%2F186074.The_Name_of_the_Wind&psig=AOvVaw3GRVm_rkrempksrbCXuGue&ust=1643819519935000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMjV3NP43vUCFQAAAAAdAAAAABAJ'
+        },
+        {
+          title: 'The Wise Mans Fear',
+          description: 'Tale of Kvothe, as he ages',
+          image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fcianpepper%2Fa-wise-mans-fear%2F&psig=AOvVaw1Ml6A428N_F8rOSsHdIv6q&ust=1643820083281000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLin_Kb53vUCFQAAAAAdAAAAABAF'
+        },
+        {
+          title: 'The Slow Regard of Silent Things',
+          description: 'Tale of Auri',
+          image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.goodreads.com%2Fbook%2Fshow%2F21535271-the-slow-regard-of-silent-things&psig=AOvVaw0oEq68ibLQ52DUNWX2xTaH&ust=1643820264388000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKiltPv53vUCFQAAAAAdAAAAABAD'
+        }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <main className='App'>
+        <h1>Book Club</h1>
+        <p>Welcome!</p>
+      </main>
+    )
+  }
 }
 
-export default App;
+export default App 
